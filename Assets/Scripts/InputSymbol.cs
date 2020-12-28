097ac3c606282d8883c05a7e3130106776ffc2ba
OleksandrKupchuk <55634677+OleksandrKupchuk@users.noreply.github.com>
2020-12-28 18:59:24 +0200

    Initial commit

---
+++ Assets/Scripts/InputSymbol.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class InputSymbol : MonoBehaviour
+{
+    [SerializeField] private InputField input;
+    //[SerializeField] private TextMeshProUGUI input;
+    [SerializeField] private string startSymbol;
+    [SerializeField] private string nameButton;
+    [SerializeField] private TextMeshProUGUI symbol;
+
+    void Start()
+    {
+        gameObject.transform.name = nameButton;
+        Debug.Log("name = " + transform.GetChild(0).transform.name);
+        symbol.text = startSymbol;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void GetSymbol()
+    {
+        input.text += symbol.text;
+        Debug.Log("symbol = " + symbol.text);
+    }
+}
